9e2e55859dc6e8410ae921d63dbf006a774acde9
anhany <and.hanc@gmail.com>
2015-12-25 11:53:10 -0700

    Add vimrc file.

---
+++ home/.vimrc.sh
@@ -0,0 +1,24 @@
+set nocompatible
+
+" Syntax highlighting
+set t_Co=256
+set background=dark
+syntax on
+colorscheme molotov
+
+
+" Other settings
+set autoindent
+set backspace=indent,eol,start
+set cursorline
+set expandtab
+set foldenable
+set foldmethod=syntax
+set foldminlines=0
+set foldnestmax=5
+set laststatus=2
+set lazyredraw
+set nu
+set smarttab
+set softtabstop=2
+
